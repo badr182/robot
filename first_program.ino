9656d892b5bfe6dce328cd0b04b3b1db19c06ca8
badr182 <badrakkar182@gmail.com>
2019-12-30 11:14:05 +0100

    Second commit: add explication

--- first_program.ino
+++ first_program.ino
@@ -28,41 +28,21 @@ void setup() {
   
   // pinMode(button, INPUT);
   // Set initial rotation direction
+  // explication par badr akkar  : 
+  // in1 = Low et in2 = High : moteur 1 et 2 en avant 
+  // si non en arrière 
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
 
-  
+  // explication par badr akkar  :
+  // intialiser le speed  
   digitalWrite(enA,SPEED);
   digitalWrite(enB,SPEED);
 }
 
-void loop() {
- //  int potValue = 255 ;// analogRead(A0); // Read potentiometer value
-  // int pwmOutput = map(1023, 0, 1023, 0 , 255); // Map the potentiometer value from 0 to 255
-  // analogWrite(enA, pwmOutput); // Send PWM signal to L298N Enable pin
-
-  // Read button - Debounce
-  // if (digitalRead(button) == true) {
-    //pressed = !pressed;
-  //}
-  // while (digitalRead(button) == true);
-  delay(20);
-
-  // If button is pressed - change rotation direction
-  //if (pressed == true  & rotDirection == 0) {
-    //digitalWrite(in1, HIGH);
-    //digitalWrite(in2, LOW);
-    //rotDirection = 1;
-    //delay(20);
-  //}  
-  // If button is pressed - change rotation direction
-  //if (pressed == false & rotDirection == 1) {
-    //digitalWrite(in1, LOW);
-    //digitalWrite(in2, HIGH);
-    //rotDirection = 0;
-    //delay(20);
-  //}
+void loop() {delay(20);
+  
 }
